9110a97010f91239f26d0dc0b3a212468084a869
Dat Nguyen <dat.peter.nguyen1@gmail.com>
2025-03-05 05:52:20 -0600

    Committing final commit

--- oss.c
+++ oss.c
@@ -6,9 +6,14 @@
 #include <sys/wait.h>
 #include <sys/shm.h> // For shared memory
 #include <sys/ipc.h> // Also for shared memory, allows worker class to access shared memory
+#include <time.h>
 
 #define SHM_KEY 854038
 #define NANO_TO_SEC 1000000000 
+
+// Author: Dat Nguyen
+// 03/4/2025
+// oss.c is the main program that creates shared memory and have the SimulatedClock knowledge, Program explained in ./oss -h
 
 // Using a structure for our simulated clock, storing seconds and nanoseconds.
 typedef struct SimulatedClock {
@@ -32,16 +37,76 @@ void printTable(SimulatedClock *clock);
 void help();
 
 int main(int argc, char **argv) {
+	int userInput = 0;
+	int childProcess = 3;
+	int simul = childProcess;
+	int timeLimit = 5;
+	int interval = 100;
+	int totalProc = 0;
+	int currentProc = 0;
+	int status;
+	pid_t pid;
+	// These variables are specifically for our table.
+	int checkSec = 0;
+	int checkNano = 0;
+	// Variables for Interval
+	int lastLaunchSec = 0;
+	int lastLaunchNano = 0;
+
+	// User Input handler
+	while ((userInput = getopt(argc, argv, "n:s:t:i:h")) != -1) {
+		switch(userInput) {
+			case 'n': // How many child processes to launch.
+				childProcess = atoi(optarg);
+				if (childProcess <= 0) {
+					printf("Error: Total child processes must be at least one. \n");
+					exit(1);
+				}
+				break;
+			case 's': // How many simulations to run at once.
+				simul = atoi(optarg);
+				// Ensuring simulations isn't zero or below for the program to work.
+				if (simul < 0) {
+					printf("Error: Simulations must be positive. \n");
+					exit(1);
+				}	
+				break;
+			case 't': // Upper bound of when calling worker process
+				timeLimit = atoi(optarg);
+				if (timeLimit <= 0) {
+                                        printf("Error: Time limit must be above one. \n");
+                                        exit(1);
+                                }
+                                break;
+
+			case 'i': // How often to launch child interval
+				interval = atoi(optarg);
+				if (childProcess <= 0) {
+					printf("Error: interval must be positive. \n");
+                                        exit(1);
+                                }
+                                break;
+			case 'h': // Prints out help function.
+				help();
+				return 0;
+			case '?': // Invalid user argument handling.
+				printf("Error: Invalid argument detected \n");
+				printf("Usage: ./oss.c -h to learn how to use this program \n");
+				exit(1);
+		}
+	}
+
+
 	int shmid = shmget(SHM_KEY, sizeof(SimulatedClock), IPC_CREAT | 0666); // Creating shared memory using shmget. 
     	
 	if (shmid == -1) { // If shmid is -1 as a result of shmget failing and returning -1, error message will print.
-        	perror("shmget failed");
+        	printf("Error: shmget failed. \n");
         	exit(1);
     	}
 	
 	SimulatedClock *clock = (SimulatedClock *)shmat(shmid, NULL, 0); // Attach shared memory, clock is now a pointer to SimulatedClock structure.
 	if (clock == (void *)-1) { // if shmat, the attaching shared memory function, fails, it returns an invalid memory address.
-        	perror("shmat failed");
+        	printf("Error: shared memory attachment failed. \n");
         	exit(1);
     	}
 
@@ -49,57 +114,113 @@ int main(int argc, char **argv) {
     	clock->seconds = 0;
     	clock->nanoseconds = 0;
 
-	incrementClock(clock); // Incrementing clock
+	// Initialize table
+	for (int i = 0; i < 20; i++) {
+		processTable[i].occupied = 0;
+        	processTable[i].pid = 0;
+        	processTable[i].startSeconds = 0;
+       		processTable[i].startNano = 0;
+	}
 
-	// Grab start time of fork and initialize PCB array index
-	int forkSec = clock->seconds;
-	int forkNano = clock->nanoseconds;
-	int pcbIndex = 0;
+	// Start the timer for 60 real seconds.
+	signal(SIGALRM, (void(*)(int))signalHandler);
+	alarm(60);
 
-	// For loop looks through and finds if any pcb slots are free.
-	for (pcbIndex = 0; pcbIndex < 20; pcbIndex++) {
-		if(!processTable[pcbIndex].occupied) {
+	srand(time(NULL));
+
+	while(1) { // OSS loop
+		incrementClock(clock);
+
+		pid_t killedPid = waitpid(-1, &status, WNOHANG); // Non blocking check for terminated child.
+
+		while (killedPid > 0) { // If there is a killed pid, make the PCB slot it occupies free.
+			for(int i = 0; i < 20; i++) {
+				if (processTable[i].occupied && processTable[i].pid == killedPid) {
+					processTable[i].occupied = 0;
+					currentProc--; // Decreasing to indicate that a process has been killed.
+				}
+			}
+
+			killedPid = waitpid(-1, &status, WNOHANG);
+		}
+		
+		// When dealing with something as big as nano seconds (1 * 10^9), you should use long.
+		long currentTimeNano = (long)clock->seconds * NANO_TO_SEC + clock->nanoseconds;
+        	long lastPrintNano = (long)checkSec * NANO_TO_SEC + checkNano;
+		
+		// Check to see if the difference between the current time and the time last print >= 0.5 seconds which is 5 followed by 9 zeros in nanoseconds.
+		if (currentTimeNano - lastPrintNano >= 500000000) {
+			printTable(clock);
+            		checkSec = clock->seconds;
+            		checkNano = clock->nanoseconds;
+        	}
+		// Child launching process, ensure that we have not reached total processes ran and that we are not exceeding sim.
+                if ((totalProc < childProcess) && (currentProc < simul)) {
+                        // Check to see if time since last child process launch exceeds interval
+                        long lastLaunchTimeNano = (long)lastLaunchSec * NANO_TO_SEC + lastLaunchNano;
+                        if ((currentTimeNano - lastLaunchTimeNano) >= (long)interval * 1000000 ) {
+				// Find empty slot in PCB
+				int pcbIndex = -1;
+				for (int i = 0; i < 20; i++) {
+					if (!processTable[i].occupied) {
+						pcbIndex = i;
+						break;
+					}
+				}
+				if (pcbIndex != -1) { // If found free PCB slot
+                                	pid = fork();
+
+					if (pid < 0) { // If fork fails
+						printf("Error: Fork Failed");
+						exit(1);
+					}
+					else if (pid == 0) {
+                                        	// We are initializing random inputs for ./worker which takes in sec and nanosec
+                                        	int randomSec = (rand() % timeLimit) + 1;
+                                        	int randomNano = rand() % NANO_TO_SEC;
+
+                                        	// Converting our inputs for ./worker to strings since that's what the argument will be expecting.
+                                        	char secStr[20];
+                                        	char nanoStr[20];
+                                        	// snprintf is safer than sprintf when dealing with large values and possible overflow.
+                                        	snprintf(secStr, sizeof(secStr), "%d", randomSec);
+                                        	snprintf(nanoStr, sizeof(nanoStr), "%d", randomNano);
+
+                                        	// Execute child function
+                                        	execl("./worker", "worker", secStr, nanoStr, (char *)NULL);
+					}
+					else {
+					       	// Update process table
+                                        	processTable[pcbIndex].occupied = 1;
+                                        	processTable[pcbIndex].pid = pid;
+                                        	processTable[pcbIndex].startSeconds = clock->seconds;
+                                        	processTable[pcbIndex].startNano = clock->nanoseconds;
+                                        	currentProc++;
+                                       		totalProc++;
+
+                                        	// Update last launch time
+                                        	lastLaunchSec = clock->seconds;
+						lastLaunchNano = clock->nanoseconds;
+					}
+				}
+			}
+		}
+		// Check if launched all child process and that there are no currently running processes.
+		if ((totalProc == childProcess) && (currentProc ==0)) {
 			break;
 		}
-	}
-	
-	// If pcbIndex is at 20, that means that there is a maximum amount of processes.
-	if (pcbIndex = 20) {
-		printf("Max Processes Detected.");
-	}
-
-   	pid_t pid = fork(); // Forking, creating new child process.
-    	
-	if (pid < 0) { // If pid fails, return error message.
-        	perror("fork failed");
-        	exit(1);
-    	}
-    	else if (pid == 0) { // Child executes worker
-        	execl("./worker", "worker", (char *)NULL);
-        	perror("execl failed");
-        	exit(1);
-    	} else { // What Parent does
-		processTable[pcbIndex].occupied = 1;
-		processTable[pcbIndex].pid = pid;
-		processTable[pcbIndex].startSeconds = forkSec;
-		processTable[pcbIndex].startNano = forkNano;
-		printTable(clock);
-	}
-
-	wait(NULL); // Wait function 
-
-	if (pid > 0) { // Free up occupation of pcb slot, ready for reuse.
-		processTable[pcbIndex].occupied = 0;
 	}
 
 	// Detach shared memory
     	if (shmdt(clock) == -1) {
-        	perror("shmdt failed");
+        	printf("Error: Shared memory detachment failed");
+		exit(1);
     	}	
 
     	// Remove shared memory
     	if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-        	perror("shmctl failed");
+        	printf("Error: Removing memory failed");
+		exit(1);
     	}
 	
 	return 0;
@@ -117,18 +238,18 @@ void incrementClock(SimulatedClock *clock) { // This function simulates the incr
 void printTable(SimulatedClock *clock) { // This function prints out the info about the 20 processes.
 	printf("\nOSS PID: %d SysClockS: %d SysClockNano: %d\n", getpid(), clock->seconds, clock->nanoseconds);
 	printf("Process Table: \n");
-	printf("Entry \t Occupied \t PID \t StartS \t StartN\n");
+	printf("Entry \t Occupied \t PID \t\t StartS \t StartN\n");
 	for (int i = 0; i < 20; i++) {
-		printf("%d \t %d \t %d \t %d \t %d \n", i, processTable[i].occupied, processTable[i].pid, processTable[i].startSeconds, processTable[i].startNano);
+		printf("%d \t %d \t\t %d \t\t %d\t\t %d \n", i, processTable[i].occupied, processTable[i].pid, processTable[i].startSeconds, processTable[i].startNano);
 	}
 }
 
 void signalHandler(int sig, int shmid, SimulatedClock* clock) { // This is our signal handler, if 60 real seconds have passed, then kill all child processes and exit.
-	fprintf(stderr, "\n[OSS] Alarm signal caught, killing all children...\n");
+	fprintf(stderr, "\n[OSS] Alarm signal caught, terminating all processes.\n");
     	
-	for(int i = 0; i < 20 ; i++){
+	for(int i = 0; i < 20; i++){
         	if(processTable[i].occupied) {
-		       	kill(processTable[i].pid,SIGTERM);
+		       	kill(processTable[i].pid, SIGTERM);
 		}
     	}
 
@@ -136,5 +257,11 @@ void signalHandler(int sig, int shmid, SimulatedClock* clock) { // This is our s
 }
 
 void help() {
-	printf("Hi");
+	printf("This program forks and splits off worker processes. It also controls the simulated clock and each loop, it increments. See below for parameters. \n")
+	printf("Usage: ./oss [-h] [-n proc] [-s simul] [-t timelimit] [-i intervalMs]\n");
+    	printf("-h : Show this help message.\n");
+    	printf("-n : Total number of child processes to launch.\n");
+    	printf("-s : Maximum number of children to run simultaneously.\n");
+    	printf("-t : Upper bound (seconds) for random child runtime generation.\n");
+    	printf("-i : Interval in milliseconds to wait before launching another child.\n");
 }
--- worker.c
+++ worker.c
@@ -6,6 +6,11 @@
 #include <sys/types.h>
 
 #define SHM_KEY 854038
+#define NANO_TO_SEC 1000000000
+
+// Author: Dat Nguyen
+// Date: 03/04/2025
+// worker.c is a program that is called as a child process from oss.c. It prints info every second about the PID, PPID, the workers lifetime, and info about simulatedClock.
 
 // SimulatedClock structure that will be used as our system clock required in project.
 typedef struct SimulatedClock {
@@ -15,28 +20,76 @@ typedef struct SimulatedClock {
 
 int main(int argc, char** argv) {
 	
-	// Using shmget to get shared memory between oss.c and worker.c 
-	int shmid = shmget(SHM_KEY, sizeof(SimulatedClock), 0666);
+	// Argument checking, this should not be true, if true, something is wrong in oss.c
+	if (argc < 3) {
+		printf("Error: two arguments expected \n");
+		exit(1);
+	}
+
+	int secLimit = atoi(argv[1]);
+	int nanoLimit = atoi(argv[1]);
+
+	// Attach to the shared memory (created by oss.c)
+    	int shmid = shmget(SHM_KEY, sizeof(SimulatedClock), 0666);
+    	if (shmid == -1) {
+        	printf("Error: attachment failed \n");
+        	exit(1);
+	}
 	
-	if (shmid == -1) { // Error handling if shmget fails.
-        	perror("shmget failed");
+	// Pointer to shared memory, getting access to SimulatedClock that oss.c has.
+	SimulatedClock* clockSHM = (SimulatedClock*) shmat(shmid, NULL, 0);
+    	if (clockSHM == (void*)-1) {
+		printf("Error: pointing to shm failed. \n");
+        	exit(1);
+	}
+
+	// time is a SimulatedClock type that holds the second and nanosecond from the SimulatedClock from oss.c
+	SimulatedClock time;
+	time.seconds = clockSHM->seconds;
+	time.nanoseconds = clockSHM->nanoseconds;
+
+	// lifeSec and targ is the lifetime of the worker, 
+	int lifeSec = time.seconds + secLimit;
+	int lifeNano = time.nanoseconds + nanoLimit;
+
+	// In case during the addition of nanoseconds, the sum is more than or equal to a second.
+	if (lifeNano >= NANO_TO_SEC) {
+		lifeSec++;
+		lifeNano -= NANO_TO_SEC;
+	}
+	
+	// Initial print
+	printf("\nWORKER PID: %d PPID: %d SysClockS: %d SysClockNano: %d TermTimeS: %d TermTimeNano: %d --Just Starting\n", getpid(), getppid(), time.seconds, time.nanoseconds, lifeSec, lifeNano );
+	// Variables in main loop. 
+	int lastTime = time.seconds; 
+	int secPassed = 0;
+
+	while(1) {
+		// Keeping track of the time passed.
+		int currentSec = clockSHM->seconds;
+		int currentNano = clockSHM->nanoseconds;
+
+		// If current sec, the simulated clock, is > than lifeSec, the lifetime of the worker, then terminate. Secondary condition in case first isn't fulfilled but still over lifetime.
+		if ((currentSec > lifeSec) || ((currentSec >= lifeSec) && currentNano >= lifeNano)) {
+			break;
+		}
+
+		if (currentSec != lastTime) { // If simulated clock changes, print this message.
+			secPassed = currentSec - time.seconds;
+			printf("\nWORKER PID: %d PPID: %d SysClockS: %d SysClockNano: %d TermTimeS: %d TermTimeNano: %d --%d seconds have passed\n", getpid(), getppid(), currentSec, currentNano, lifeSec, lifeNano, secPassed);
+			lastTime = currentSec; // Updating lastTime for the next print message.
+		}
+	}
+
+	// Printing final message after termination
+	printf("\nWORKER PID: %d PPID: %d SysClockS: %d SysClockNano: %d TermTimeS: %d TermTimeNano: %d --Termination\n", getpid(), getppid(), clockSHM->seconds, clockSHM->nanoseconds, lifeSec, lifeNano);
+
+	// Detach shared memory
+    	if (shmdt(clockSHM) == -1) {
+        	perror("Error: Detached Failed \n");
         	exit(1);
     	}
 
-	SimulatedClock *clockSHM = (SimulatedClock *)shmat(shmid, NULL, 0); // clockSHM is a pointer to the shared memory where the SimulatedClock, which was put in the shared memory by oss.c, resides.
-
-	if (clockSHM == (void *)-1) { // Error handling if clockSHM fails.
-        	perror("shmat failed");
-        	exit(1);
-    	}	
-	
-	printf("Child: Current Time: %d sec %d ns\n", clockSHM->seconds, clockSHM->nanoseconds); // If working, the program will print out the seconds and nano seconds data from 
-
-	if (shmdt(clockSHM) == -1) { // If detachment of shared memory fails, print error message.
-        	perror("shmdt failed");
-        	exit(1);
-    	}
-
-	return 0;
+	return 0;	
 }
 
