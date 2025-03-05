68df2bf4f7734e54a4ea014e8372339786cd0376
Dat Nguyen <dat.peter.nguyen1@gmail.com>
2025-03-04 19:22:16 -0600

    Added a function that simulates incrementing the clock.

--- oss.c
+++ oss.c
@@ -8,13 +8,25 @@
 #include <sys/ipc.h> // Also for shared memory, allows worker class to access shared memory
 
 #define SHM_KEY 854038
+#define NANO_TO_SEC 1000000000 
 
 // Using a structure for our simulated clock, storing seconds and nanoseconds.
 typedef struct SimulatedClock {
-	int seconds;
-	int nanoseconds;
+        int seconds;
+        int nanoseconds;
 } SimulatedClock;
 
+
+// Structure for our PCB table
+struct PCB {
+	int occupied;
+	pid_t pid;
+	int startSeconds;
+	int startNano;
+};
+struct PCB processTable[20]; // Table of PCBstruct PCB processTable[MAX_PROCESSES];
+
+void incrementClock(SimulatedClock *clock);
 
 int main(int argc, char **argv) {
 	int shmid = shmget(SHM_KEY, sizeof(SimulatedClock), IPC_CREAT | 0666); // Creating shared memory using shmget. 
@@ -31,8 +43,10 @@ int main(int argc, char **argv) {
     	}
 
     	// Initialize clock
-    	clock->seconds = 1;
-    	clock->nanoseconds = 1000000;
+    	clock->seconds = 0;
+    	clock->nanoseconds = 0;
+
+	incrementClock(clock);
 
    	pid_t pid = fork(); // Forking, creating new child process.
     	
@@ -57,5 +71,15 @@ int main(int argc, char **argv) {
     	if (shmctl(shmid, IPC_RMID, NULL) == -1) {
         	perror("shmctl failed");
     	}	
+	
+	return 0;
 }
 
+void incrementClock(SimulatedClock *clock) { // This function simulates the increment of our simulated clock.
+	clock->nanoseconds += 100000; // Start increasing in milliseconds
+
+	while (clock->nanoseconds >= NANO_TO_SEC) { // Expect even after reducing nano seconds to have a bit of remaining nano seconds, while ensures that if they build up, that they'll be reduced properly.
+		clock->seconds++;
+		clock->nanoseconds -= NANO_TO_SEC;	
+	}
+}
